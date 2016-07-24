f4b434ab3e94f2673039ac2acd3f0e887de71b45
broele <broelemeter@web.de>
2016-07-24 20:50:14 +0200

    Add a package overview with some sample tags

---
+++ src/de/broele/taglets/ref/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains a taglets for managing references in JavaDoc comments.
+ * To make use of these tags, use JavaDoc with the options:<br>
+ * {@code -taglet de.broele.taglets.ref.ReferenceTag -taglet de.broele.taglets.ref.CiteTag -tagletpath path/to/the/refTags.jar}
+ * <br>
+ * Further information can be found in the web{@cite taglet}.
+ *
+ * @ref taglet Taglet Overview from Oracle. http://docs.oracle.com/javase/7/docs/technotes/guides/javadoc/taglet/overview.html
+
+ */
+package de.broele.taglets.ref;
